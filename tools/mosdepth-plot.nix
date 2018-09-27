fc17098fd20b49ce72756b8eb25c7a09f2ed4434
Justin Bedo <cu@cua0.org>
2018-09-28 09:57:19 +1000

    Change mosdepth plotting to multisample

--- tools/mosdepth-plot.nix
+++ tools/mosdepth-plot.nix
@@ -1,15 +1,22 @@
 { stdenv
+, lib
 , mosdepth
 , python
 , flags ? null}:
 
-input:
+with lib;
+
+{ inputs
+, names ? []}:
 
 stdenv.mkDerivation {
   name = "mosdepth-depth";
   buildInputs = [ python ];
   buildCommand = ''
-    python ${mosdepth.src}/scripts/plot-dist.py ${input}/*global.dist.txt
+    python ${mosdepth.src}/scripts/plot-dist.py ${concatMapStringsSep " " (i: "${i}/out.mosdepth.global.dist.txt") inputs}
+    ${concatStringsSep "\n" (zipListsWith (i: n: "substituteInPlace dist.html --replace ${i}/out ${n}") inputs names)}
+    substituteInPlace dist.html --replace "x: 0.1" "x: 0.9"
+    substituteInPlace dist.html --replace "y: 0.1" "y: 0.9"
     mv dist.html $out
   '';
 }
