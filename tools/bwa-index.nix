a75e95881c49cfa52cc47c27a4980b2381cc0fbf
Justin Bedo <cu@cua0.org>
2018-09-27 15:41:09 +1000

    Allow flag passing to bwa index

--- tools/bwa-index.nix
+++ tools/bwa-index.nix
@@ -1,6 +1,7 @@
 { stdenv
 , lib
 , bwa
+, flags ? null
 }:
 
 ref:
@@ -12,7 +13,7 @@ stdenv.mkDerivation {
   buildInputs = [ bwa ];
   buildCommand = ''
     ln -s ${ref} ref.fa
-    bwa index ref.fa
+    bwa index ${optionalString (flags != null) flags} ref.fa
     mkdir $out
     mv ref.fa.* $out
     grep '^>[^ \t]*_alt$' ref.fa | tr -d '^>' > $out/idxbase.alt || true
