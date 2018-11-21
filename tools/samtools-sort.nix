98e79735080b379e9a674a69771707951bbdc1ac
Justin Bedo <cu@cua0.org>
2018-11-22 10:44:16 +1100

    samtools-sort: handle missing 'none' sorting case

--- tools/samtools-sort.nix
+++ tools/samtools-sort.nix
@@ -19,7 +19,7 @@ assert (matchFiletype "samtools-sort" { bam = _: true; sam = _: true; cram = _: 
 let
   outfmtR = if outfmt != null then outfmt input else input.filetype;
   outFmtFlags = matchFiletype "samtools-sort-outfmt" { bam = _: "-O BAM"; sam = _: "-O SAM"; cram = ref: "-O CRAM -T ${ref}"; } {filetype = outfmtR;};
-  alreadySorted = matchFileSorting "samtools-sort" { name = _: nameSort; coord = _: !nameSort; } input;
+  alreadySorted = matchFileSorting "samtools-sort" { name = _: nameSort; coord = _: !nameSort; none = _: false;} input;
 in stdenv.mkDerivation {
   name = "samtools-sort";
   buildInputs = [ samtools ];
