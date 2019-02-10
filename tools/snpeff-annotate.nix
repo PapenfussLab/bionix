bb7d6dce2f5f9ad4ebc39a80eff9e4cf66b34a28
Justin Bedo <cu@cua0.org>
2019-02-11 09:11:36 +1100

    snpeff: parameterise heap size and default to 31g

--- tools/snpeff-annotate.nix
+++ tools/snpeff-annotate.nix
@@ -1,5 +1,6 @@
 {bionix
 ,db
+,heapSize ? "31g"
 ,flags ? ""}:
 
 input:
@@ -12,6 +13,7 @@ assert (matchFiletype "snpeff-annotate" { vcf = _: true; } input);
 stage {
   name = "snpeff-annotate";
   buildInputs = with pkgs; [ snpeff ];
+  JAVA_TOOL_OPTIONS = "-Xmx${heapSize}";
   buildCommand = ''
     ln -s ${db} ${db.name}
     snpeff -nodownload -dataDir $TMPDIR ${db.name} ${input} > $out
--- tools/snpeff-dbnsfp.nix
+++ tools/snpeff-dbnsfp.nix
@@ -1,5 +1,6 @@
 {bionix
 ,dbnsfp
+,heapSize ? "31g"
 ,flags ? ""}:
 
 input:
@@ -11,6 +12,7 @@ assert (matchFiletype "snpeff-dbnsfp" { vcf = _: true; } input);
 
 stage {
   name = "snpeff-dbnsfp";
+  JAVA_TOOL_OPTIONS = "-Xmx${heapSize}";
   buildCommand = ''
     ln -s ${dbnsfp.db} dbNSFP.txt.gz
     ln -s ${dbnsfp.index} dbNSFP.txt.gz.tbi
