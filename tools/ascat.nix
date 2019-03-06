be9238d8266682925ae4ad4d291621c03ac38dfd
Justin Bedo <cu@cua0.org>
2019-03-06 13:25:52 +1100

    api docs: init ascatNGS

--- tools/ascat.nix
+++ tools/ascat.nix
@@ -4,6 +4,14 @@ with bionix;
 
 {
   app = pkgs.callPackage ./ascat-app.nix {};
+
+  /* Generate GC correction file for ascatNGS.
+  Type: {ref :: fasta, chrPrefix :: string, ...} -> (snps :: VCF) -> (gc :: GC)
+  */
   gccorrect = callBionixE ./ascat-gccorrect.nix;
+
+  /* Call CNVs using ascatNGS. Gender is a string as per ascatNGS docs (e.g., "XX").
+  Type: {ref :: fasta, gc :: GC, ...} -> {tumour :: bam, normal :: bam, gender :: string} -> CNVs
+  */
   callCNV = callBionixE ./ascat-callCNV.nix;
 }
