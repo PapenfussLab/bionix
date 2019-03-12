d5bfd9c5595d012634807045681aa90621dd8b23
Justin Bedo <cu@cua0.org>
2019-03-13 09:17:35 +1100

    mosdepth: fix broken pkg link for mosdepth source

--- tools/mosdepth-plot.nix
+++ tools/mosdepth-plot.nix
@@ -12,7 +12,7 @@ stage {
   name = "mosdepth-depth";
   buildInputs = with pkgs; [ python ];
   buildCommand = ''
-    python ${mosdepth.src}/scripts/plot-dist.py ${concatMapStringsSep " " (i: "${i}/out.mosdepth.global.dist.txt") inputs}
+    python ${pkgs.mosdepth.src}/scripts/plot-dist.py ${concatMapStringsSep " " (i: "${i}/out.mosdepth.global.dist.txt") inputs}
     ${concatStringsSep "\n" (zipListsWith (i: n: "substituteInPlace dist.html --replace ${i}/out ${n}") inputs names)}
     substituteInPlace dist.html --replace "x: 0.1" "x: 0.9"
     substituteInPlace dist.html --replace "y: 0.1" "y: 0.9"
