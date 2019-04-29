5d03ab8afa1803ddabc7616a48fff9889e3a0c51
Justin Bedo <cu@cua0.org>
2019-04-30 09:52:01 +1000

    fix example data urls and hashes

--- examples/default.nix
+++ examples/default.nix
@@ -9,34 +9,34 @@ let
     # Sample 1
     {
       input1 = fetchFastQ {
-        url = "https://github.com/PapenfussLab/bionix/raw/master/example/sample1-1.fq";
-        sha256 = "1m3vc248mbr4v56459q4xsklznssgqb35lwhwk1i9qvxqm7c72nq";
+        url = "https://github.com/PapenfussLab/bionix/raw/master/examples/sample1-1.fq";
+        sha256 = "0kh29i6fg14dn0fb1xj6pkpk6d83y7zg7aphkbvjrhm82braqkm8";
       };
 
       input2 =  fetchFastQ {
-      url = "https://github.com/PapenfussLab/bionix/raw/master/example/sample1-2.fq";
-        sha256 = "13fqvdb5r2sidi2i0s3ifg8gyxp8kibpxc3cbiw07d5zcn3g479x";
+      url = "https://github.com/PapenfussLab/bionix/raw/master/examples/sample1-2.fq";
+        sha256 = "0czk85km6a91y0fn4b7f9q7ps19b5jf7jzwbly4sgznps7ir2kdk";
       };
     }
 
     # Sample 2
     {
       input1 = fetchFastQ {
-        url = "https://github.com/PapenfussLab/bionix/raw/master/example/sample2-1.fq";
-        sha256 = "0c6ik4smsw2kb8xbbci80d00c24klk6mqd2a71y4v9hmpnc42fmr";
+        url = "https://github.com/PapenfussLab/bionix/raw/master/examples/sample2-1.fq";
+        sha256 = "08gixavfklqvk1m2ic6v56z82vl00qnpsd9xb64z6zl03nz98mcy";
       };
 
       input2 =  fetchFastQ {
-        url = "https://github.com/PapenfussLab/bionix/raw/master/example/sample2-2.fq";
-        sha256 = "0zvrkm2m69lwfi89hzxsbnkp4i6cszh7c624l1qwmd4s6gh7vfcx";
+        url = "https://github.com/PapenfussLab/bionix/raw/master/examples/sample2-2.fq";
+        sha256 = "1xxwm2vq52axpdhm14rh5mg5nzzpxaqnvhzrqhajm27fqksgzjjw";
       };
     }
   ];
 
   # The reference for the synthetic data
   ref = fetchFastA {
-    url = "https://github.com/PapenfussLab/bionix/raw/master/example/ref.fa";
-    sha256 = "06gphhh40h3mvwvs2m51qc3rpih8mcs8frhd48l94d5bzwfhb2hc";
+    url = "https://github.com/PapenfussLab/bionix/raw/master/examples/ref.fa";
+    sha256 = "0sy9hq8n55knfkiblam50dzaiwhrx6pv8b8l1njdn6kfj4wflz2p";
   };
 
 in import ./call.nix {inherit inputs ref;}
