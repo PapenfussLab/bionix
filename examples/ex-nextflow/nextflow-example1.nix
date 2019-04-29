a1d18efc18772a233aa759b622c3a9960824f109
Justin Bedo <cu@cua0.org>
2019-04-30 09:47:49 +1000

    cleanup examples and stray files

---
+++ examples/ex-nextflow/nextflow-example1.nix
@@ -0,0 +1,30 @@
+# This is a translation of the Nextflow example found at
+# https://www.nextflow.io/example1.html
+{ bionix ? import <bionix> {}
+, input ? ./sample.fa}:
+
+with bionix;
+with lib;
+
+let
+  splitSequences = fa: stage {
+    name = "splitSequences";
+    buildInputs = [ pkgs.gawk ];
+    buildCommand = ''
+      awk '/^>/{f="seq_"++d} {print > f}' ${fa}
+      mkdir $out
+      cp seq* $out
+    '';
+  };
+
+  reverse = fa: stage {
+    name = "reverse";
+    buildCommand = ''
+      ${pkgs.utillinux}/bin/rev ${fa} > $out
+    '';
+  };
+
+in pipe [
+  splitSequences
+  (each reverse)
+] input
---
+++ examples/ex-wdl/wdl-scatter-gather.nix
@@ -0,0 +1,33 @@
+# The scatter-gather example from https://github.com/openwdl/wdl
+# translated to bionix
+{ bionix ? import <bionix> {} }:
+
+with bionix;
+with lib;
+
+let
+
+  prepare = splitString "\n" (removeSuffix "\n" (readFile (stage {
+    name = "prepare";
+    buildInputs = [ pkgs.python3 ];
+    buildCommand = ''
+      python -c "print('one\ntwo\nthree\nfour', end=''')" > $out
+    '';
+  })));
+
+  analysis = str: removeSuffix "\n" (readFile (stage {
+    name = "analysis";
+    buildInputs = [ pkgs.python ];
+    buildCommand = ''
+      python -c "print('_${str}_')" > $out
+    '';
+  }));
+
+  gather = strs: stage {
+    name = "gather";
+    buildCommand = ''
+      echo ${concatStringsSep " " strs} > $out
+    '';
+  };
+
+in gather (map analysis prepare)
