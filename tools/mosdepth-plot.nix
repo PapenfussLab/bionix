e7a6aef11516c1330ae21610b4e14321b137fd04
Justin Bedo <cu@cua0.org>
2019-07-03 15:27:50 +1000

    mosdepth-plot: allow selection of what to plot
    Previously global output was plotted. A new control flag allows
    specification of what to plot.

--- tools/mosdepth-plot.nix
+++ tools/mosdepth-plot.nix
@@ -1,4 +1,5 @@
 { bionix
+, what ? "global"
 , flags ? null
 }:
 
@@ -12,8 +13,8 @@ stage {
   name = "mosdepth-depth";
   buildInputs = with pkgs; [ python ];
   buildCommand = ''
-    python ${pkgs.mosdepth.src}/scripts/plot-dist.py ${concatMapStringsSep " " (i: "${i}/out.mosdepth.global.dist.txt") inputs}
-    ${concatStringsSep "\n" (zipListsWith (i: n: "substituteInPlace dist.html --replace ${i}/out ${n}") inputs names)}
+    python ${pkgs.mosdepth.src}/scripts/plot-dist.py ${concatMapStringsSep " " (i: "${i}/out.mosdepth.${what}.dist.txt") inputs}
+    ${concatStringsSep "\n" (zipListsWith (i: n: "substituteInPlace dist.html --replace ${i}/out.mosdepth.${what}.dist.txt ${n}") inputs names)}
     substituteInPlace dist.html --replace "x: 0.1" "x: 0.9"
     substituteInPlace dist.html --replace "y: 0.1" "y: 0.9"
     mv dist.html $out
