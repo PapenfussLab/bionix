bc92717d8960f918b7903dab9315f068c705fe4c
Justin Bedo <cu@cua0.org>
2019-08-16 09:32:56 +1000

    mosdepth: fix renaming

--- tools/mosdepth-plot.nix
+++ tools/mosdepth-plot.nix
@@ -10,11 +10,11 @@ with lib;
 , names ? []}:
 
 stage {
-  name = "mosdepth-depth";
+  name = "mosdepth-plot";
   buildInputs = with pkgs; [ python ];
   buildCommand = ''
     python ${pkgs.mosdepth.src}/scripts/plot-dist.py ${concatMapStringsSep " " (i: "${i}/out.mosdepth.${what}.dist.txt") inputs}
-    ${concatStringsSep "\n" (zipListsWith (i: n: "substituteInPlace dist.html --replace ${i}/out.mosdepth.${what}.dist.txt ${n}") inputs names)}
+    ${concatStringsSep "\n" (zipListsWith (i: n: "sed -i 's|${i}[^ ]*|${n}|' dist.html") inputs names)}
     substituteInPlace dist.html --replace "x: 0.1" "x: 0.9"
     substituteInPlace dist.html --replace "y: 0.1" "y: 0.9"
     mv dist.html $out
