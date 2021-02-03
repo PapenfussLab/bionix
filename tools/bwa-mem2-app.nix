0985153cd011b412c0e2e34c99acdefca1399cc8
Justin Bedo <cu@cua0.org>
2021-02-03 11:48:25 +1100

    bwa-mem2: 2.0 -> 2.1

--- tools/bwa-mem2-app.nix
+++ tools/bwa-mem2-app.nix
@@ -1,18 +1,18 @@
 { clangStdenv, fetchFromGitHub, zlib }:
   clangStdenv.mkDerivation rec {
   pname = "bwa-mem2";
-  version = "2.0";
+  version = "2.1";
   src = fetchFromGitHub {
     fetchSubmodules = true;
     owner = "bwa-mem2";
     repo = "bwa-mem2";
     rev = "v${version}";
-    sha256 = "0q5wqal0nfxd3yfbmxahyaiqqmsrrplnwhplcjvz1xzw7bxwwnnj";
+    sha256 = "sha256-T0nkO+NehAMFuwGi7HrQxq57gEsA16TlEWa2SHerxV4=";
   };
   buildInputs = [ zlib ];
   installPhase = ''
     mkdir -p $out/bin
-    cp bwa-mem2 $out/bin
+    cp bwa-mem2 bwa-mem2.* $out/bin
   '';
   disableHardening = [ "pie" ];
 }
