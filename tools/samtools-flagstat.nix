0a637afd8bc82d03e474295895251aa47323d747
Justin Bedo <cu@cua0.org>
2022-01-04 08:21:06 +1100

    samtools-flagstat: fix name

--- tools/samtools-flagstat.nix
+++ tools/samtools-flagstat.nix
@@ -7,7 +7,7 @@ with bionix;
 with lib;
 
 stage {
-  name = "samtools-index";
+  name = "samtools-flagstat";
   buildInputs = with pkgs; [ samtools ];
   buildCommand = "samtools flagstat -@ $NIX_BUILD_CORES ${input} > $out";
   passthru.multicore = true;
